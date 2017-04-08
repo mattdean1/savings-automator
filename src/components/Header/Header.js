c4a2550b3cae7b232181015a8a7b2fcf23a80137
Calum Bell <kxcomp@gmail.com>
2017-04-08 11:52:44 +0100

    Added New Page, added styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,19 @@
-import React from "react";
-import {Link} from "react-router";
-import {Menu, Image} from "semantic-ui-react";
-import logo from "../../assets/safe-logo.png";
+import React from 'react'
+import { Link } from 'react-router'
+import { Menu, Image, Icon } from 'semantic-ui-react'
+import logo from '../../assets/safe-logo.png'
 
 export const Header = () => (
   <div>
-    <Menu fixed="top" inverted>
-      <Link to="/">
-      <Menu.Item className="headerPadding">
-        <Image src={logo} className="ui image App-logo" alt="logo" width='60px'/>
-        <p className='brand'>AllSafe</p>
-      </Menu.Item>
-      </Link>
-      <Menu.Menu position="right">
-        <Menu.Item onClick={e => window.location.href = '/api/logout'}>
-          <h4>Logout</h4>
+    <Menu fixed='top' inverted>
+      <Link to='/'>
+        <Menu.Item className=''>
+          <Image src='' className='ui image' alt='Back' width='60px' />
+          <p className='brand'>Automatic Savings</p>
         </Menu.Item>
-      </Menu.Menu>
+      </Link>
     </Menu>
   </div>
-);
+)
 
 export default Header
--- src/routes/Sandbox/views/SandboxView.js
+++ src/routes/Sandbox/views/SandboxView.js
@@ -1,11 +1,11 @@
-import React from "react";
-import URLSearchParams from "url-search-params";
-import {Loader, Grid, Button, Header, Container, Message, Segment, Icon} from "semantic-ui-react";
-import Dashboard from "../../../components/Dashboard/Dashboard";
-import {Link} from 'react-router'
+import React from 'react'
+import URLSearchParams from 'url-search-params'
+import { Loader, Grid, Button, Header, Container, Message, Segment, Icon } from 'semantic-ui-react'
+import Dashboard from '../../../components/SavingDash/Savings'
+import { Link } from 'react-router'
 import UserDenied from '../../../components/UserDenied/UserDenied'
-import QuickTable from '../../../components/QuickTable';
-import {transactionsProjection, transactionsSelection} from '../../../components/TransactionTable/TransactionTable';
+import QuickTable from '../../../components/QuickTable'
+import { transactionsProjection, transactionsSelection } from '../../../components/TransactionTable/TransactionTable'
 
 class SandboxView extends React.Component {
 
@@ -13,14 +13,14 @@ class SandboxView extends React.Component {
     loadTransactions: React.PropTypes.func.isRequired,
     loadBalance: React.PropTypes.func.isRequired,
     loadCustomer: React.PropTypes.func.isRequired,
-    setLoading: React.PropTypes.func.isRequired,
+    setLoading: React.PropTypes.func.isRequired
   };
 
   componentWillMount () {
-    this.props.setLoading(true);
-    this.props.loadTransactions();
-    this.props.loadCustomer();
-    this.props.loadBalance();
+    this.props.setLoading(true)
+    this.props.loadTransactions()
+    this.props.loadCustomer()
+    this.props.loadBalance()
   }
 
   componentWillUnmount () {
@@ -28,19 +28,18 @@ class SandboxView extends React.Component {
   }
 
   render () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const error = urlParams.get('error');
-    const {loading, transactions, balance, customer} = this.props.sandbox;
+    const urlParams = new URLSearchParams(window.location.search)
+    const error = urlParams.get('error')
+    const { loading, transactions, balance, customer } = this.props.sandbox
     return (
       <Grid>
-        <br/>
-        {loading ? <Loading/>
-          : ( transactions && balance && customer ?
-            <Dashboard mode={'Sandbox'} customer={customer} transactions={transactions} balance={balance}>
-              <QuickTable projection={transactionsProjection} selection={transactionsSelection} items={transactions} />
-            </Dashboard> :
-            <AnonymousProfile />)}
-        {error && error === 'access_denied' ? <UserDenied/> : null}
+        {loading ? <Loading />
+          : (transactions && balance && customer
+            ? <Dashboard mode={'Sandbox'} customer={customer} transactions={transactions} balance={balance}>
+              {/* <QuickTable projection={transactionsProjection} selection={transactionsSelection} items={transactions} /> */}
+            </Dashboard>
+            : <AnonymousProfile />)}
+        {error && error === 'access_denied' ? <UserDenied /> : null}
       </Grid>
     )
   }
@@ -48,18 +47,18 @@ class SandboxView extends React.Component {
 
 const Loading = () => {
   return (
-    <Loader active size="large"/>
-  );
-};
+    <Loader active size='large' />
+  )
+}
 
 const AnonymousProfile = () => {
   return (
     <Container>
-      <Link to="/">
+      <Link to='/'>
         <Button>{`< Back`}</Button> </Link>
-      <Segment size="large" textAlign="center">
-        <Header as="h2" icon={true}>
-          <Icon name="warning sign"/>
+      <Segment size='large' textAlign='center'>
+        <Header as='h2' icon>
+          <Icon name='warning sign' />
           Access Denied
           <Header.Subheader>
             Check the sandbox refresh token in the server config is valid and try again.
@@ -68,7 +67,34 @@ const AnonymousProfile = () => {
       </Segment>
     </Container>
 
-  );
-};
+  )
+}
+
+const goals = () => {
+  return (
+    <Container>
+      <h1>Goals</h1>
+    </Container>
+
+  )
+}
+
+const plans = () => {
+  return (
+    <Container>
+      <h1>Plans</h1>
+    </Container>
+
+  )
+}
+
+const rules = () => {
+  return (
+    <Container>
+      <h1>Rules</h1>
+    </Container>
+
+  )
+}
 
 export default SandboxView
